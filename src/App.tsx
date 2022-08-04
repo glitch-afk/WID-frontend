d3db42dc6e34893bb9fb398fef91f1d1321d605f
Satyam Kulkarni <punekar.satyam@gmail.com>
2022-08-04 11:39:54 +0530

    feat: added db.ts for google spreadsheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import ClaimID from "./components/ClaimID";
 import Hero from "./components/Hero";
+import { db } from "./utils/db";
 
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 function App() {
-  const [userID, setUserID] = useState("");
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes>
-        <Route
-          path="/"
-          element={<Hero userID={userID} setUserID={setUserID} />}
-        />
-        <Route path="/claim" element={<ClaimID userID={userID} />} />
-      </Routes>
-      <Toaster />
-    </div>
-  );
+	const [userID, setUserID] = useState("");
+
+	useEffect(() => {
+		db("");
+	}, []);
+
+	return (
+		<div className="App">
+			<Navbar />
+			<Routes>
+				<Route
+					path="/"
+					element={<Hero userID={userID} setUserID={setUserID} />}
+				/>
+				<Route path="/claim" element={<ClaimID userID={userID} />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
